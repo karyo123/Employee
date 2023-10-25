03057c9cdddf6a3b76cd3c5b3b8e530eb6b50edf
karyo123 <sakaryo10@gmail.com>
2023-10-25 15:51:50 +0530

    Another three methods are added

--- src/test/java/org/git/com/FirstSession.java
+++ src/test/java/org/git/com/FirstSession.java
@@ -5,14 +5,25 @@ public class FirstSession {
 	
 	public void Tc1() {
 		
-
+         System.out.println("10");
 	}
 	public void Tc2() {
 		
+		
+	System.out.println("20");	
 
 	}
 	public void Tc3() {
 		
+     System.out.println("30");
+	}
+	
+	public void Tc4() {
+		System.out.println("40");
+
+	}
+	public void Tc5() {
+		System.out.println("50");
 
 	}
 }
